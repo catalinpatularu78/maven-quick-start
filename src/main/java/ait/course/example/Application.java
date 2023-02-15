d770dfcf07af4b4bb0ce68b1f011d594c830fae6
catalinpatularu78 <A00304504@student.ait.ie>
2023-02-15 09:51:09 +0000

    Use string utils and dependency to lang3

--- src/main/java/ait/course/example/Application.java
+++ src/main/java/ait/course/example/Application.java
@@ -1,16 +1,23 @@
-package ait.course.example;
+package tus.course.example;
 import java.util.ArrayList;
 import java.util.List;
+import org.apache.commons.lang3.StringUtils;
 
 public class Application {
- 
-   public void greet(){
+	
+	public int countWords(String words){
+		String[] separateWords=StringUtils.split(words, ' ');
+		return (separateWords == null)? 0 : separateWords.length;
+	}
+	
+	public void greet(){
 		List<String> greetings = new ArrayList<>();
 		greetings.add("Hello");
 		for (String greeting: greetings){
 			System.out.println("Greeting: "+greeting);
 		}
-	}	
+	}
+ 
     public Application() {
         System.out.println("Inside Application");
     }
@@ -20,5 +27,7 @@ public class Application {
     	System.out.println ("Starting Application");
 		Application app = new Application();
 		app.greet();
+		int count = app.countWords("I have four words");
+		System.out.println("Word Count: "+count);
     }
 }
